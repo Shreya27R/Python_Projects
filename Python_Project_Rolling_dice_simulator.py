52b405a4f02e291f4e83dd1331daffbe144359a8
unknown <shreyaborker99@gmail.com>
2022-09-17 21:13:24 +0530

    Final commit

--- Python_Project_Rolling_dice_simulator.py
+++ Python_Project_Rolling_dice_simulator.py
@@ -4,14 +4,35 @@ import random
 from tkinter import *
 
 root = Tk() #referes to tkinter window
-root.geometry("600x300")
+root.geometry("600x600")
 
-text_1 = Label(root,text='',font=("arial",300))
+label = Label(root,text='',font=("arial",300))
+label_desc = Label(root,text='',font=("arial",10))
 
 def roll_the_dice():
-    num_code =['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685',]
-    text_1.config(text=f'{random.choice(num_code)}{random.choice(num_code)}')
-    text_1.pack()
+    value =['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685',]
+    result= random.choice(value)
+    label.configure(text=result)
+    label.pack()
+    if result =='\u2680':
+        label_desc.configure(text='You rolled One! Roll the dice again')
+        label_desc.place(x=200,y=450)
+    elif result =='\u2681':
+        label_desc.configure(text='You rolled Two! Roll the dice again')
+        label_desc.place(x=200,y=450)
+    elif result =='\u2682':
+        label_desc.configure(text='You rolled Three! Roll the dice again')
+        label_desc.place(x=200,y=450)
+    elif result =='\u2683':
+        label_desc.configure(text='You rolled Four! Roll the dice again')
+        label_desc.place(x=200,y=450)
+    elif result =='\u2684':
+        label_desc.configure(text='You rolled Five! Roll the dice again')
+        label_desc.place(x=200,y=450)
+    else:
+        label_desc.configure(text='You rolled Six! Roll the dice again')
+        label_desc.place(x=200,y=450)
+
 
 
 button_1 =Button(root,text="Roll the dice",command=roll_the_dice)
